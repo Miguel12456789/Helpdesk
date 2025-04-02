135bc34826681632e234bb2606971cc5d8d443cc
Miguel Martins <luizm0027@gmail.com>
2025-04-02 16:38:39 +0100

    lol

--- src/router/navegationController.js
+++ src/router/navegationController.js
@@ -8,5 +8,9 @@ const renderPage = (page) => (req, res) => {
 
 const home = renderPage("home");
 const base_gov = renderPage("base_gov")
+const estatisticas_setor_publico = renderPage("estatisticas_setor_publico")
+const estatisticas_operadores_economicos = renderPage("estatisticas_operadores_economicos")
+const oportunidade_contratacao_publica = renderPage("oportunidade_contratacao_publica")
+const outros = renderPage("outros")
 
-module.exports = { home, base_gov };
+module.exports = { home, base_gov, estatisticas_setor_publico, estatisticas_operadores_economicos, oportunidade_contratacao_publica, outros };
