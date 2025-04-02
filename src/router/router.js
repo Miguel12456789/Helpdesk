a7ba7c391fb07b2dca73aaae7b4142a9d63b3e25
Miguel Martins <luizm0027@gmail.com>
2025-04-02 12:57:26 +0100

    update nao sei

--- src/router/router.js
+++ src/router/router.js
@@ -3,6 +3,5 @@ const router = express.Router();
 const navegationController = require('./navegationController');
 
 router.get("/", navegationController.home);
-router.get("/contract_details", navegationController.contract_details);
 
 module.exports = router;
