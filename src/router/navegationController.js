60ebf461e545c6ebe69b9f8f104bd808e46f95b2
Miguel Martins <luizm0027@gmail.com>
2025-04-09 14:33:38 +0100

    Update

--- src/router/navegationController.js
+++ src/router/navegationController.js
@@ -12,6 +12,5 @@ const estatisticas_setor_publico = renderPage("estatisticas_setor_publico")
 const estatisticas_operadores_economicos = renderPage("estatisticas_operadores_economicos")
 const oportunidade_contratacao_publica = renderPage("oportunidade_contratacao_publica")
 const outros = renderPage("outros")
-const detalhes = renderPage("detalhes")
 
-module.exports = { home, base_gov, estatisticas_setor_publico, estatisticas_operadores_economicos, oportunidade_contratacao_publica, outros, detalhes };
+module.exports = { home, base_gov, estatisticas_setor_publico, estatisticas_operadores_economicos, oportunidade_contratacao_publica, outros };
--- src/router/router.js
+++ src/router/router.js
@@ -8,6 +8,5 @@ router.get("/estatisticas_setor_publico", navegationController.estatisticas_seto
 router.get("/estatisticas_operadores_economicos", navegationController.estatisticas_operadores_economicos);
 router.get("/oportunidade_contratacao_publica", navegationController.oportunidade_contratacao_publica);
 router.get("/outros", navegationController.outros);
-router.get("/detalhes", navegationController.detalhes);
 
 module.exports = router;
