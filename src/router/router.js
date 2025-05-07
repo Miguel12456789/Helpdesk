6242ed97878398a7a3e9d2602ce7f35df8a0c30b
Miguel Martins <luizm0027@gmail.com>
2025-05-07 15:31:13 +0100

    y

--- src/router/router.js
+++ src/router/router.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const navegationController = require('./navegationController');
 
 router.get("/", navegationController.home);
+
 router.get("/base_gov", navegationController.base_gov);
 
 
